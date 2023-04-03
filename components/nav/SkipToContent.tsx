eaf612ad44b1b05a70e14a7c833780071965d9bd
innovatedev-john-pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-04-03 11:27:36 -0600

    Update SkipToContent.tsx

--- components/nav/SkipToContent.tsx
+++ components/nav/SkipToContent.tsx
@@ -1,13 +1,7 @@
-import { tw } from "twind";
-
-const SkipToContent = ({ target = "main-content" }: { target: string }) => {
-  const skipToContentClass = tw`skip-to-content`;
-
+export default function SkipToContent({ target = "main-content" }: { target: string }) {
   return (
-    <a href={`#${target}`} className={skipToContentClass}>
+    <a href={`#${target}`} class="absolute top-[-40px] left-0 bg-white text-black p-2 z-9999 text-sm transition:top duration-300 ease-in-out focus:top-0">
       Skip to Content
     </a>
   );
-};
-
-export default SkipToContent;
+}
