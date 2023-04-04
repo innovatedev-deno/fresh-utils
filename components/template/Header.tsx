6a5b41ca83547c50192bd04af977a0ec4d36bb56
innovatedev-john-pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-04-04 00:39:28 -0600

    Create Header.tsx

---
+++ components/template/Header.tsx
@@ -0,0 +1,22 @@
+import { tw } from "twind";
+import MobileNav from "~/islands/MobileNav.tsx";
+
+export default function({title}: {title?: string}) {
+  const styles = {
+    logo: tw`font-bold text-xl p-4 whitespace-nowrap flex items-center`,
+    logoImg: tw`inline-block mr-4 w-[48px]`,
+  }
+  return <>
+    <a href="/" class={styles.logo}>
+      <img src="/logo.svg" />
+      {title}
+    </a>
+    
+    <MobileNav links={[
+      { href: "/blog", text: "Blog" },
+      { href: "/contact", text: "Contact" },
+      // TODO: share context with this component to handle auth nav
+      { href: "/login", text: "Login" },
+    ]} />
+  </>
+}
