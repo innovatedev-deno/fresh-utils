c8957e36033905cca2e2b9d142dfb2b454858e37
innovatedev-john-pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-04-04 00:45:39 -0600

    Create settings-public.ts

---
+++ settings-public.ts
@@ -0,0 +1,11 @@
+// example settings file, copy to ~/public-settings.ts and update values for your site.
+// !!! Contents of this file is visible publicly when deployed. !!!
+//
+export default {
+  recaptcha: {
+    siteId: "",
+  },
+  app: {
+    title: "My Site",
+  }
+};
