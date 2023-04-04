1a341fe1c7d05724f6e2973ee97720682500e04f
innovatedev-john-pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-04-04 00:51:59 -0600

    Create theme.ts

---
+++ twind/theme.ts
@@ -0,0 +1,17 @@
+export const customColors = {
+  primary: {
+    DEFAULT: "#5C4033",
+    light: "#A8755D",
+    dark: "#422E24",
+  },
+  secondary: {
+    DEFAULT: "#4C80A8",
+    light: "#6EBAF5",
+    dark: "#2F5069",
+  },
+  highlight: {
+    DEFAULT: "#375C43",
+    light: "#8BE8A9",
+    dark: "#284230",
+  }
+};
