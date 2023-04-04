62fe4b130537398517d35566b96adce0c07ffdba
innovatedev-john-pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-04-03 22:57:13 -0600

    Delete SkipToContent.tsx

--- components/nav/SkipToContent.tsx
+++
@@ -1,7 +0,0 @@
-export default function SkipToContent({ target = "main-content" }: { target: string }) {
-  return (
-    <a href={`#${target}`} class="absolute top-[-40px] left-0 bg-white text-black p-2 z-9999 text-sm transition:top duration-300 ease-in-out focus:top-0">
-      Skip to Content
-    </a>
-  );
-}
