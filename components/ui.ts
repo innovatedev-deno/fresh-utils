1ef450ad35d44a0d5e5421e32730297df78786b4
innovatedev-john-pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-04-05 01:52:53 -0600

    Create ui.ts

---
+++ components/ui.ts
@@ -0,0 +1,6 @@
+import TestimonialCard from "~/components/ui/TestimonialCard.tsx";
+import { TestimonialKeys } from "~/data/testimonials.ts";
+
+export const CarouselCardMap = new Map([
+  [TestimonialKeys.join(','), TestimonialCard],
+])
