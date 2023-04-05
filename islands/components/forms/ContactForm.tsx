4618e6879a87b4f00f53892d8b9c79d48b01f14b
innovatedev-john-pope <88814278+innovatedev-john-pope@users.noreply.github.com>
2023-04-04 18:45:47 -0600

    Create ContactForm.tsx

---
+++ islands/components/forms/ContactForm.tsx
@@ -0,0 +1,79 @@
+import ReCaptchaForm from "fresh-utils/components/forms/ReCaptchaForm.tsx";
+import publicSettings from "~/settings-public.ts";
+
+export default function (
+  { type = "contact", errors, isSuccess, data }: {
+    errors?: Record<string, Record<string, string>>;
+    isSuccess?: boolean;
+    type?: "schedule" | "contact";
+    data?: { name: string; email: string; message: string };
+  },
+) {
+  const recaptchaKey = publicSettings.recaptcha.siteId;
+  const action = type == "schedule"
+    ? "schedule_appointment_submission"
+    : "contact_form_submission";
+
+  return <ReCaptchaForm
+    class="w-full"
+    method="post"
+    recaptchaKey={recaptchaKey}
+    action={action}
+  >
+    {isSuccess
+      ? (
+        <div className="info-success">
+          Thank you for your submission. We will be in touch with you
+          shortly.
+        </div>
+      )
+      : errors &&
+        (
+          <div class="">
+            {Object.entries(errors).map(([field, error]) =>
+              Object.entries(error).map(([rule, message]) => (
+                <div>{message}</div>
+              ))
+            )}
+          </div>
+        )}
+
+    <input type="hidden" name="type" value={type} />
+
+    <label>
+      Name
+      <input
+        name="name"
+        type="text"
+        required
+        value={data?.name}
+      />
+    </label>
+
+    <label>
+      Email
+      <input
+        type="email"
+        name="email"
+        value={data?.email}
+      />
+    </label>
+
+    <label for="message" class="">
+      Message
+      <textarea
+        name="message"
+        rows={5}
+        required
+      >
+        {data?.message}
+      </textarea>
+    </label>
+
+    <div class="flex">
+      <button type="submit">
+        Send
+      </button>
+    </div>
+  </ReCaptchaForm>
+}
